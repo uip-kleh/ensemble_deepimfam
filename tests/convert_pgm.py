c76963cdb475240b58d77980da68975abf5218f5
uipkleh <h23ms416@hirosaki-u.ac.jp>
2024-05-08 12:15:54 +0900

    move to test

--- tests/convert_pgm.py
+++ tests/convert_pgm.py
@@ -1,6 +1,6 @@
 from new_deepimfam.component import ImageGenerator
 
 if __name__ == "__main__":
-    image_gen = ImageGenerator(config_path="new_deepimfam/config.yaml")
+    image_gen = ImageGenerator(config_path="../new_deepimfam/config.yaml")
     image_gen.convert_pgm()
     
--- tests/draw_vectors.py
+++ tests/draw_vectors.py
@@ -1,7 +1,7 @@
 from new_deepimfam.component import ImageGenerator
 
 if __name__ == "__main__":
-    image_gen = ImageGenerator(config_path="new_deepimfam/config.yaml")
+    image_gen = ImageGenerator(config_path="../new_deepimfam/config.yaml")
     vectors = image_gen.generate_normed_verctors()
     image_gen.draw_vectors(vectors)
     image_gen.make_images_info()
